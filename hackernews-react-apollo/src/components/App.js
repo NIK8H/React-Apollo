e19e951185ad0ac95c39bbb14e39ae2c56bae9ab
MeenakshiKathiresan <meenakshikathiresan3@gmail.com>
2023-05-07 17:36:43 -0400

    added routes

--- hackernews-react-apollo/src/components/App.js
+++ hackernews-react-apollo/src/components/App.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
 import LinkList from './linklist.component';
+import CreateLink from './createlink.component';
+import Header from './header.component';
+
+import { Route, Routes } from 'react-router-dom';
 
 class App extends Component {
   render() {
-    return <LinkList />;
+    return <div>
+      <Header />
+      <div className="ph3 pv1 background-gray">
+        <Routes>
+          <Route path="/" element={<LinkList/>} />
+          <Route
+            path="/create"
+            element={<CreateLink/>}
+          />
+        </Routes>
+      </div>
+      </div>;
   }
 }
 
